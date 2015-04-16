ce8f3cf52532f697b94982a8679dc5ea2693159b
PoisonAlien <anandmt3@gmail.com>
2015-04-16 17:32:10 +0800

    changed the way bigwig files read

--- plot_chipPeaks.R
+++ plot_chipPeaks.R
@@ -1,4 +1,4 @@
-plotPeaks = function(bigWig,peaks_bed,chr,start,end,isBigWigFile = FALSE,showGeneStruct = FALSE){
+plotPeaks = function(bigWig,peaks_bed,chr,start,end,isBigWigFile = TRUE,showGeneStruct = TRUE){
   
   library(rtracklayer)
   library(ggplot2)
@@ -44,33 +44,32 @@ plotPeaks = function(bigWig,peaks_bed,chr,start,end,isBigWigFile = FALSE,showGen
   
   #################################################################################################################################################
   
-  if(isBigWigFile){
-    #read bigwig file
-    print(paste("Reading ",bigWig,sep = ":"))
-    bw = import.bw(bigWig)  
-  }
-  
-  #read peaks file (in bed format)
-  peaks = read.delim(peaks_bed,header = F,stringsAsFactors = F)
-  #convert it into GRanges object.
-  peaks = GRanges(seqnames = peaks[,1],ranges = IRanges(start = peaks[,2],end = peaks[,3]))
-  
   #form a GRanges area for plotting area.
   plot.region = GRanges(seqnames = chr,ranges = IRanges(start = c(start),end = c(end)))
+  
+  #read peaks file (in bed format)
+  peaks = import.bed(con = peaks_bed,which = plot.region)
+  
+  if(isBigWigFile){
+    #read bigwig file
+    bw = import.bw(con = bigWig,selection = plot.region)
+  }
   
   #Get scores for regions to plot.
   reg = subsetByOverlaps(query = bw,subject = plot.region)
   reg.df = data.frame(midpoint = start(reg)+(end(reg) - start(reg))/2,score = reg$score)
-  
+    
   #Get peaks to highlight.
   peaksToShow = as.data.frame(subsetByOverlaps(query = peaks,subject = plot.region))
   
   #plot main peaks
-  p1 = ggplot(reg.df, aes(x=midpoint, y=score)) +geom_area(colour="black", fill="blue", alpha=.2)+ylim(0,max(reg.df$score)+2)+xlab("position")+ylab("rpm")+theme_bw()+theme_minimal()+theme(axis.line = element_line(colour = "black"),panel.grid.major=element_blank())+xlim(start,end)
+  a = ggplot(reg.df, aes(x=midpoint, y=score)) +geom_area(colour="black", fill="blue", alpha=.2)+ylim(0,max(reg.df$score)+2)+xlab("position")+ylab("rpm")+theme_bw()+theme_minimal()+theme(axis.line = element_line(colour = "black"),panel.grid.major=element_blank())+xlim(start,end)
   
   #plot bed regions
-  p2 = ggplot(data = peaksToShow,aes(ymin = -0.5,ymax = 0.5,xmin = start,xmax = end,aplha = 0.2))+geom_rect()+theme(axis.line=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks=element_blank(),axis.title.x=element_blank(),axis.title.y=element_blank(),legend.position="none",panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),panel.grid.minor=element_blank(),plot.background=element_blank())
-  p2 = p2 + xlim(start,end)
+  b = ggplot(data = peaksToShow,aes(ymin = -0.3,ymax = 0.3,xmin = start,xmax = end,aplha = 0.2))+geom_rect()+theme(axis.line=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks=element_blank(),axis.title.x=element_blank(),axis.title.y=element_blank(),legend.position="none",panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),panel.grid.minor=element_blank(),plot.background=element_blank())
+  b = b + xlim(start,end) + ylim(-1,1)
+  
+  pl = list(b,a)
   
   if(showGeneStruct){
     
@@ -79,22 +78,33 @@ plotPeaks = function(bigWig,peaks_bed,chr,start,end,isBigWigFile = FALSE,showGen
     genome(mySession) = "hg19"
     
     #Download all genes which lie in the plot region.
-    geneStruct = getTable(ucscTableQuery(mySession, track="RefSeq Genes",range=plot.region, table="refGene"))
-    geneStruct = geneStruct[1,] #For now this will only plot the fisrt entry :(
+    geneTable = getTable(ucscTableQuery(mySession, track="RefSeq Genes",range=plot.region, table="refGene"))
+    #geneStruct = geneStruct[1,] #For now this will only plot the fisrt entry :(
     
-    #Get start and end locus of each exon and make a data frame.
-    exon.start = as.numeric(unlist(strsplit(x = as.character(geneStruct$exonStarts),split = ",",fixed = T)))
-    exon.end = as.numeric(unlist(strsplit(x = as.character(geneStruct$exonEnds),split = ",",fixed = T)))
-    exon.df = data.frame(start = as.numeric(exon.start),end = as.numeric(exon.end),gene = as.character(geneStruct$name2),exon = paste("exon",1:length(exon.start),sep = "_"),stringsAsFactors = F)
+    #form a layout
+    nrow = 7
+    layout = matrix(c(1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2), nrow = nrow, byrow = TRUE)
     
-    #plot the gene structure.
-    p3 = ggplot(data = exon.df,aes(ymin = -0.5,ymax = 0.5,xmin = start,xmax = end,aplha = 0.2))+geom_rect()+ylim(-2,1)+xlim(start,end)+geom_rect(data = geneStruct,aes(ymin = -0.1,ymax = 0.1,xmin = txStart,xmax = txEnd,aplha = 0.2))+theme(axis.line=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks=element_blank(),axis.title.x=element_blank(),axis.title.y=element_blank(),legend.position="none",panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),panel.grid.minor=element_blank(),plot.background=element_blank())
-    p3 = p3+annotate("text",label = as.character(geneStruct$name2),x = start+500,y = -1) #Label it.
+    for( i in 1:nrow(geneTable)){
+      
+      layout = rbind(layout,rep(i+2,3))
+      
+      geneStruct = geneTable[i,]
+      
+      #Get start and end locus of each exon and make a data frame.
+      exon.start = as.numeric(unlist(strsplit(x = as.character(geneStruct$exonStarts),split = ",",fixed = T)))
+      exon.end = as.numeric(unlist(strsplit(x = as.character(geneStruct$exonEnds),split = ",",fixed = T)))
+      exon.df = data.frame(start = as.numeric(exon.start),end = as.numeric(exon.end),gene = as.character(geneStruct$name2),exon = paste("exon",1:length(exon.start),sep = "_"),stringsAsFactors = F)
+      
+      #plot the gene structure.
+      x = ggplot(data = exon.df,aes(ymin = -0.5,ymax = 0.5,xmin = start,xmax = end,aplha = 0.2))+geom_rect()+ylim(-1.5,1)+xlim(start,end)+geom_rect(data = geneStruct,aes(ymin = -0.1,ymax = 0.1,xmin = txStart,xmax = txEnd,aplha = 0.2))+theme(axis.line=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks=element_blank(),axis.title.x=element_blank(),axis.title.y=element_blank(),legend.position="none",panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),panel.grid.minor=element_blank(),plot.background=element_blank())
+      x = x +annotate("text",label = as.character(geneStruct$name2),x = start+1000,y = -1,size = 4) #Label it.
+      
+      pl[[2+i]] = x
+      rm(x)
+    }
     
-    #form a layout for plotting.
-    layout = matrix(c(1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3), nrow = 8, byrow = TRUE)
-    
-    multiplot(plotlist = list(p2,p1,p3), layout = layout)
+    multiplot(plotlist = pl, layout = layout)
   } else{
     layout = matrix(c(1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2), nrow = 7, byrow = TRUE)
     
